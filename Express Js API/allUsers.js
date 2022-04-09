a34814ef186397d07005b6aea6598cce24a1312e
ammad877 <ammad.ullah877@gmail.com>
2022-04-09 16:24:35 +0500

    node-js

---
+++ Express Js API/allUsers.js
@@ -0,0 +1,53 @@
+const users = [{
+    "id": 1,
+    "first_name": "Ricardo",
+    "last_name": "Gumb",
+    "email": "rgumb0@vk.com"
+  }, {
+    "id": 2,
+    "first_name": "Issi",
+    "last_name": "Glanvill",
+    "email": "iglanvill1@msn.com"
+  }, {
+    "id": 3,
+    "first_name": "Judi",
+    "last_name": "Seamons",
+    "email": "jseamons2@ask.com"
+  }, {
+    "id": 4,
+    "first_name": "Erma",
+    "last_name": "Regenhardt",
+    "email": "eregenhardt3@macromedia.com"
+  }, {
+    "id": 5,
+    "first_name": "Ines",
+    "last_name": "Bodimeade",
+    "email": "ibodimeade4@nbcnews.com"
+  }, {
+    "id": 6,
+    "first_name": "Katrina",
+    "last_name": "Ardron",
+    "email": "kardron5@addthis.com"
+  }, {
+    "id": 7,
+    "first_name": "Burtie",
+    "last_name": "Eleshenar",
+    "email": "beleshenar6@purevolume.com"
+  }, {
+    "id": 8,
+    "first_name": "Seymour",
+    "last_name": "Entwistle",
+    "email": "sentwistle7@homestead.com"
+  }, {
+    "id": 9,
+    "first_name": "Nata",
+    "last_name": "Gilardi",
+    "email": "ngilardi8@biblegateway.com"
+  }, {
+    "id": 10,
+    "first_name": "Jamaal",
+    "last_name": "Galloway",
+    "email": "jgalloway9@google.fr"
+  }]
+
+  module.exports = users;
